6fcd6160b4b2eaea587bba77cd9cb05368dc1867
brandongomez <brandon@gomezland.com>
2025-08-05 20:23:40 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 
 // Connect to Heroku Postgres
 const pool = new Pool({
